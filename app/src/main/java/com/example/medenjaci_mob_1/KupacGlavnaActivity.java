71c3f754d74d93d62986672bc7bae900539627a2
samed-hamidovic <hamidovic34@gmail.com>
2021-03-11 23:36:29 +0100

    add dots for slider

--- app/src/main/java/com/example/medenjaci_mob_1/KupacGlavnaActivity.java
+++ app/src/main/java/com/example/medenjaci_mob_1/KupacGlavnaActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class KupacGlavnaActivity extends AppCompatActivity {
 
     ViewPager mSlideViewPager;
     LinearLayout mDotsLayout;
+
+    TextView[] mDots;
 
     SliderAdapter sliderAdapter;
     @Override
@@ -28,5 +32,43 @@ public class KupacGlavnaActivity extends AppCompatActivity {
         sliderAdapter = new SliderAdapter(this);
         mSlideViewPager.setAdapter(sliderAdapter);
 
+        addDotIndicator(0);
+        mSlideViewPager.addOnPageChangeListener(viewListener);
     }
+
+    public void addDotIndicator(int position){
+
+        mDots = new TextView[4];
+        mDotsLayout.removeAllViews();
+        for(int i = 0; i < mDots.length; i++){
+            mDots[i] = new TextView(this);
+
+            mDots[i].setText(Html.fromHtml("&#8226"));
+            mDots[i].setTextSize(70);
+            mDots[i].setTextColor(getResources().getColor(R.color.black));
+
+            mDotsLayout.addView(mDots[i]);
+        }
+
+        if(mDots.length > 0 ){
+            mDots[position].setTextColor(getResources().getColor(R.color.yellow_100));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotIndicator(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
